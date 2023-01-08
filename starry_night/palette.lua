4db46fb1c5010dec3074880b60929e0e2a39db1b
midnight <kevinrodriguez4851@gmail.com>
2023-01-08 01:33:29 -0600

    base colorscheme

---
+++ starry_night/palette.lua
@@ -0,0 +1,16 @@
+local colors =
+{
+  bg      = "#0f080e",
+  fg      = "#c9c9c9",
+  alt     = "#262026",
+  num     = "#edcfca",
+  str     = "#eebab2",
+  var     = "#ccaa9e",
+  err     = "#ff6961",
+  warn    = "#f89975",
+  hint    = "#ac7d88",
+  info    = "#898aa6",
+  preproc = "#54bab9",
+}
+
+return colors
