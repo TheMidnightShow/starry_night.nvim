ad3e32f604466e03bd63e8ac8cf0e7feb8b260ca
midnight <kevinrodriguez4851@gmail.com>
2023-01-08 14:24:47 -0600

    light dark colorscheme

--- lua/starry_night/colors.lua
+++ lua/starry_night/colors.lua
@@ -1,4 +1,6 @@
-local colors =
+local M = {}
+
+M.dark =
 {
   bg      = "#0f080e",
   fg      = "#c9c9c9",
@@ -11,6 +13,24 @@ local colors =
   hint    = "#ac7d88",
   info    = "#898aa6",
   preproc = "#54bab9",
+  comment = "#563d53",
 }
 
-return colors
+M.light =
+{
+  bg      = "#f0f7f1",
+  fg      = "#363636",
+  alt     = "#d9d9d9",
+  num     = "#123035",
+  str     = "#11454d",
+  var     = "#335561",
+  err     = "#ff6961",
+  warn    = "#f89975",
+  hint    = "#ac7d88",
+  info    = "#898aa6",
+  preproc = "#ab4546",
+  comment = "#a9c2ac",
+
+}
+
+return M
