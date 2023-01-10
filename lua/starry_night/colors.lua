91ec3d10087dd43187966e1e5439ac0b38247753
midnight <kevinrodriguez4851@gmail.com>
2023-01-10 15:20:57 -0600

    lualine support

--- lua/starry_night/colors.lua
+++ lua/starry_night/colors.lua
@@ -2,6 +2,7 @@ local M = {}
 
 M.dark =
 {
+  -- code highlighting
   bg      = "#0f080e",
   fg      = "#c9c9c9",
   alt     = "#262026",
@@ -14,10 +15,19 @@ M.dark =
   info    = "#898aa6",
   preproc = "#54bab9",
   comment = "#563d53",
+
+  -- lualine modes 
+  term    = "#ccaa93",
+  normal  = "#c9c9c9",
+  insert  = "#54bab9",
+  visual  = "#edcfca",
+  command = "#ac7d88",
+  replace = "#eebab2",
 }
 
 M.light =
 {
+  -- code highlighting
   bg      = "#f0f7f1",
   fg      = "#363636",
   alt     = "#d9d9d9",
@@ -31,6 +41,13 @@ M.light =
   preproc = "#cc8f90",
   comment = "#a9c2ac",
 
+  -- lualine modes 
+  term    = "#123035",
+  normal  = "#0f080e",
+  insert  = "#335561",
+  visual  = "#11454d",
+  command = "#363636",
+  replace = "#262626",
 }
 
 return M
