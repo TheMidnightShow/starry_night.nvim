350fb2007673745095c77d8f55f1d164144ae352
midnight <kevinrodriguez4851@gmail.com>
2023-01-10 15:31:30 -0600

    bold

--- lua/lualine/themes/starry_night.lua
+++ lua/lualine/themes/starry_night.lua
@@ -18,35 +18,35 @@ M.normal =
 
 M.insert =
 {
-  a = { bg = c.insert, fg = c.bg },
+  a = { bg = c.insert, fg = c.bg, bold = true },
   b = { bg = c.bg, fg = c.insert },
   c = { bg = c.bg, fg = c.insert },
 }
 
 M.command =
 {
-  a = { bg = c.command, fg = c.bg },
+  a = { bg = c.command, fg = c.bg, bold = true },
   b = { bg = c.bg, fg = c.command },
   c = { bg = c.bg, fg = c.command },
 }
 
 M.visual =
 {
-  a = { bg = c.visual, fg = c.bg },
+  a = { bg = c.visual, fg = c.bg, bold = true },
   b = { bg = c.bg, fg = c.visual },
   c = { bg = c.bg, fg = c.visual },
 }
 
 M.terminal =
 {
-  a = { bg = c.term, fg = c.bg },
+  a = { bg = c.term, fg = c.bg, bold = true },
   b = { bg = c.bg, fg = c.term },
   c = { bg = c.bg, fg = c.term },
 }
 
 M.replace =
 {
-  a = { bg = c.replace, fg = c.bg },
+  a = { bg = c.replace, fg = c.bg, bold = true },
   b = { bg = c.bg, fg = c.replace },
   c = { bg = c.bg, fg = c.replace },
 }
