e6b32959fdc129cdd17593b4a78cc23400682bb5
midnight <kevinrodriguez4851@gmail.com>
2023-01-13 19:41:18 -0600

    refactor: load themes from dir

--- lua/starry_night/init.lua
+++ lua/starry_night/init.lua
@@ -1,19 +1,16 @@
 local M = {}
-local theme = require('starry_night.theme')
 
-M.setup = function(color)
+
+M.setup = function(theme)
   vim.cmd('hi clear')
-
-  vim.o.background = color 
-
-  if vim.fn.exists('syntax_on') then
-    vim.cmd('syntax reset')
-  end
+  vim.o.background = color
+  vim.cmd('syntax reset')
 
   vim.o.termguicolors = true
   vim.g.colors_name = 'starry_night'
 
-  theme.set_highlights(color)
+  local cs = require("starry_night."..theme)
+  cs.setup()
 end
 
 return M
